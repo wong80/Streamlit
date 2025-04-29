e07520497ea2e2d1f8f4b9a8faacdabd87dc8b18
Zhi Yuan Wong <zhiyuan3112@gmail.com>
2025-04-29 17:25:19 +0800

    Revert "changed things"
    This reverts commit c59b0cc049f5b6424756b4e35e23cc3799d884db.

--- firebase.py
+++ firebase.py
@@ -3,7 +3,8 @@ password="123456"
 
 import pyrebase
 import pandas as pd
-import Ml
+import csv
+
 firebaseConfig={"apiKey":"AIzaSyC2YV31UO6dqnaqBLJ2iuMWRkt35oU5Mvo",
                 "authDomain":"fyp-powermonitoring-b2afc.firebaseapp.com",
                 "databaseURL":"https://fyp-powermonitoring-b2afc-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -11,25 +12,21 @@ firebaseConfig={"apiKey":"AIzaSyC2YV31UO6dqnaqBLJ2iuMWRkt35oU5Mvo",
                 "storageBucket":"fyp-powermonitoring-b2afc.firebasestorage.app"
 }
 
-# Initialize Firebase
-def initializeFirebase(date):
+
+def initializeFirebase():
 
     firebase = pyrebase.initialize_app(firebaseConfig)
 
     auth =firebase.auth()
     users = auth.sign_in_with_email_and_password(email, password)
     db = firebase.database()
-    users = db.child("UsersData").child("zQ7se3Lqr7ZvaYHwruZLMBssrZB2").child("readings").child(str(date)).get()
+    users = db.child("UsersData").child("zQ7se3Lqr7ZvaYHwruZLMBssrZB2").child("readings").get()
 
     return users
 
-
-# Retrieve Data from Database
 def retrieveData(users) -> pd.DataFrame:
     df = pd.DataFrame()
-    
     for user in users.each():
-        print(user.val())
         temp =pd.Series(user.val()).to_frame()
         
         df = pd.concat([df,temp],axis=1)
@@ -39,40 +36,7 @@ def retrieveData(users) -> pd.DataFrame:
     df.set_index('dateTime', inplace=True)
     df = df.drop(['timestamp'],axis=1).sort_index()
 
-    # df.to_csv('temp.csv')
+
     return(df)
-
-
-
-
-# Extract Data from specific dates and compile to csv file
-def test(start,end):
-    firebase = pyrebase.initialize_app(firebaseConfig)
-    auth =firebase.auth()
-    users = auth.sign_in_with_email_and_password(email, password)
-    db = firebase.database()
-    df = pd.DataFrame()
-
-    for i in range(start,end+1):
-        users = db.child("UsersData").child("zQ7se3Lqr7ZvaYHwruZLMBssrZB2").child("readings").child(str(i)).get()
-
-
-        for user in users.each():
-            if users.each() != None:
-                temp =pd.Series(user.val()).to_frame()
-                
-                df = pd.concat([df,temp],axis=1)
-
-            
-    df = df.transpose()
-    df['dateTime']= pd.to_datetime(df['timestamp'],dayfirst=True)
-    df.set_index('dateTime', inplace=True)
-    df = df.drop(['timestamp'],axis=1).sort_index()
-    df = df[df.index.notna()]
-    df.to_csv('temp.csv')
-    return (df)
-        
-        
-
 
 
